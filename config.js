359336dcece1c35dbf2b1f9c9daa0a3fe9ab27f8
Abhishek Shekhawat <85601866+Abhishek-S-2001@users.noreply.github.com>
2024-11-25 08:16:12 +0530

    Config added

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    JWT_Access_key: 'm4HflGFwo3fGi42Ie8md',
+    JWT_Refresh_key: 'shekhawat',
+
+    JWT_Access_Crypto_key: 'KyrBtN6JzYqe2cOU91lrswjzpcP5eMT7',
+
+    crypto_key: 'a5ca27414e9b36df13e9e491b671b75a75a72ac0ef31e0d4ba748a38607fea70',
+  };
